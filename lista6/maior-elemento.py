7b6dd46d918a59837e85628f3f787bdd9ef53d07
Jhones Bazilio dos Santos <jhones.santos@ituran.com.br>
2019-11-21 16:00:03 -0300

    melhorando exercicio maior_elemento

--- lista6/maior-elemento.py
+++ lista6/maior-elemento.py
@@ -1,7 +1,2 @@
-def maior_elemento(lista):
-    maior = lista[0]
-
-    for item in lista:
-        if item > maior:
-            maior = item
-    return maior
+def maior_elemento(lista):    
+    return sorted(lista)[-1]
